04dc8c681f910f060f35e3c7f7059f2c758879a3
lexdolfing <lexdolfing@gmail.com>
2024-04-18 10:11:40 +0200

    added all the content of the first blog post

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,10 +1,123 @@
-const blogData = [
-    {id: 1,
-        title: 'The big croissant journey: part I',
-    },
-    {},
-    {},
+import oneCoverImage from './blogImages/1/soloview_baked.HEIC';
+import oneProcessOne from './blogImages/1/unproofed_layers.HEIC';
+import oneProcessTwo from './blogImages/1/layerview_proofed.HEIC';
+import oneResultOne from './blogImages/1/topview_baked.HEIC';
 
-]
+const blogData =
+    {
+        id: 1,
+        title: 'The Home Made Vegan Croissant Journey: part I',
+        date: '11 April 2024',
+        images: {
+            coverPhoto: {
+                image: oneCoverImage,
+                description: 'Croissant side view',
+            },
+            processPhotoOne: {
+                image: oneProcessOne,
+                description: 'Croissants where you see the layers, unproofed'
+            },
+            processPhotoTwo: {
+                image: oneProcessTwo,
+                description: 'Croissants where you see the layers, proofed but not yet baked'
+            },
+            resultPhotoOne: {
+                image: oneResultOne,
+                description: 'Baked croissants on baking tray'
+            }
+        },
+        Introduction: `
+            <article>
+            <h2>Intro to the series</h2>
+            <p>Hi bakers! Welcome to my first blog post. Warning: this is going to be a long one. But it's only because I am introducing you to my biggest passion in baking: croissants. This is the first post in a series where I want to take you along in the process of mastering the art of making vegan croissants at home. I will show the recipes and methods that I used, and what I learn from changing the many factors that can make or break a croissant. Hoping to finally show you how to make a show-stopping, jaw-dropping vegan croissant at home. 
+            </p>
+            <p>Ever since I started baking, being able to make beautiful croissants was my ultimate goal. This was the food I missed the most after going vegan. Flaky, buttery, crunchy and soft at the same time, just the ultimate pastry if done right. Unfortunately, the plant-based croissants that were out there didn’t seem to do the trick. They were usually dry, not crunchy and lacking in flavor. So, I set out on a quest to make them myself. But I found out that the simple and elegant looking croissants actually require a lengthy process and a lot of skills. </p>
+            <p>Veganizing the recipe seemed quite easy at first sight. The only things that need a vegan replacement are the milk and butter in the dough, and the butter for touring. Replacing those with a plant-based milk and butter are always quite straightforward in other recipes. But the problem started with the butter for lamination. Margarine is much more breakable than dairy butter, meaning that the block of butter is broken up while laminating, creating layers that are not continuous. This greatly decreases the volume and puffiness of the croissants. So margarine requires an extra level of skill and different environmental conditions to work with.</p>    
+            <p>At home I never really achieved the results I wanted. The outcome was unpredictable, the croissants sometimes having a nice open structure but often too bread-like and lacking volume. But when I started working as a professional baker and we were developing our recipe and method for vegan croissants, I learned a lot. I got professional education, teaching me all the variables that are so important in creating a perfect croissant, that I had never heard anywhere else before. I was able to make amazing croissants with the sought-after honeycomb structure every day. Having that first croissant of the day after working so hard so early in the morning, created the happiest of moments for me.</p>
+            <p>After returning to home-baking, the results of my croissants with the same recipe didn’t turn out to be as good as I had hoped. Without a kneading machine it was hard to get the right level of gluten development in the dough. Without a lamination machine the butter would break while rolling out the dough, destroying the layers. Without a proofing cabinet the croissants would take too long to proof and without a large forced-fan oven the croissants would not get enough heat quickly enough. The home environment requires a different approach.</p>
+            <p>So this is where I am now. I can get some good layers in the croissants, but the final result is not as good as I want it. In this blog series I will explain to you the methods that I use, and the way I try to improve to make all the variables as perfect as they can be in a home environment, to make the best croissants possible. But first, let’s dive into how you actually make croissants. </p>
+            <h2>The croissant process: a quick introduction</h2>   
+            <p>A croissant dough is built up of layers of yeasted dough (pâte levée) alternated with layers of butter (feuilletée), made so by rolling a slab of butter in the dough and repeatedly folding it and rolling it out: the touring process. These layers and the fermentation of the yeast result in the super airy, crunchy exterior and soft, cloudy interior, if done right.</p>
+            <p>The dough is a slightly enriched dough with a bit of butter, milk and sugar. It is usually a fairly dry dough, since the firmness of the dough should be as close as possible to that of the cold butter, to make for even lamination. The dough should be mixed and kneaded until there is “medium” gluten development, meaning it has become somewhat smooth, and you can make the starting phase of a “window-pane”. After, the dough should be made in a square shape, packed in plastic and be placed in the freezer for at least 30 minutes, before being placed in the fridge overnight. You want to give the dough rest time for more gluten and flavor development, but you put it in the freezer to stop the fermentation process. When too many air pockets will form in the dough the lamination process becomes much more difficult.</p>
+            <p>The butter should be beaten into a rectangle of about 0.5-1 cm thickness. This makes the butter more pliable and less prone to break. The dough should then be rolled out to twice the size of the butter block, so it can encapsulate it completely. When this is done, the dough is rolled out in one direction until it is about 5 mm thick. Then the first fold starts. Depending on how many layers you want the croissant to get you can make different folds: a simple fold (tour simple) where you fold the dough in three, or a book-fold (tour double) where you fold both edges of the dough to the middle and fold it there again, to get four layers. </p>
+            <p>For the second tour, turn the dough 90 degrees (to stretch the gluten in a different direction) and start rolling out again until about 0.5 cm thickness. Do another simple fold or book fold (depending on the amount of layers you want) and either repeat that for a third time or roll out the dough to make the croissants. In between, you can rest the dough. </p>
+            <p>To roll out the dough for the final croissants, rotate the dough again 90 degrees from the last tour and roll out mostly in that direction, until you get a rectangle shape of about 20 cm wide and 0.5 cm thickness. Cut croissant shapes and roll them into croissants.</p>
+            <p>Place them under plastic and rest them overnight in the fridge (yes, this is a three day recipe). The next day, place a bowl in the bottom of the oven and fill it with about half a liter of boiling water, and let that sit for ten minutes. This will create a warm and humid condition for the croissants to proof. If you have a thermometer, make sure the temperature is between 24 and 28 degrees, and never above that. If the temperature is too high, the butter will melt out of the dough and destroy all the layers you worked so hard for. </p>
+            <p>Place the croissants on the baking tray that you will bake them in the oven. Proof them until they are “doubled” in size. You can see if they’re ready by slightly shaking the baking sheet, they should wobble a bit. Also you should see some separation between the layers in the croissants where air pockets have formed.</p>
+            <p>Get them out of the oven and preheat the oven on high temperature (200-225 degrees Celsius fan), for at least 20 minutes. Glaze the croissants with (preferably barista version) plant milk and optionally some maple syrup. Bake them at 200 degrees for about 15-18 minutes. If they brown too quickly, turn down the temperature to 175 degrees. </p>
+            <p>For my first attempt at a home baked croissant since leaving the professional bakery environment, I tried to use the recipe I knew and perfected in that same bakery. This is the recipe including baker’s percentages:</p>
+            <p>Some notes on the flour: the combination of T45 and T65 flour gives this a good mix between being a pastry and being bread. The T65 gives it more strength while the T45 gives it more airiness. If you want to follow along but cannot get your hands on these specific (french) flours, use all-purpose flour for T45 and bread flour for T65, or use only all-purpose flour. </p>
+            </article>
+            `,
+        recipe: {
+            difficulty: 'Hard',
+            cookingTime: '2 hours',
+            waitingTime: '3 days',
+            cuisine: 'French (or Austrian?)',
+            portions: '6 croissants',
+            recipeTitle: `Home-made vegan croissants part I: why don't they proof!?`,
+            ingredients: `
+            <ul>
+                <li>Flour T45 - 245g - 70%</li>
+                <li>Flour T65 - 105g - 30%</li>
+                <li>Sugar - 33g - 9%</li>
+                <li>Salt - 6.5g - 2%</li>
+                <li>Yeast (instant) - 7g - 2%</li>
+                <li>Vegan butter/margarine - 33g - 9%</li>
+                <li>Water - 66g - 19%</li>
+                <li>Milk (soy, unsweetened) - 73g - 21%</li>
+            </ul>
+            <p>Hydration of the dough: 40%</p>
+            <h2>Butter for Lamination (Naturli) - 200g - 57%</h2>
+            `,
+            method: `
+            <p>Day 1:</p>
+            <ol>
+            <li>Mixed all the ingredients for the dough. Milk and butter fridge temperature and water cold from the tap. Other ingredients at room temperature. 
+            </li>
+            <li>Kneaded the dough for 10 minutes by hand.</li>
+            <li>Made dough into a square and cooled in the freezer for 30 minutes and transferred to the fridge for overnight cooling.</li>
+            </ol>
+            
+            <p>Day 2:</p>
+            <ol>
+            <li>Slapping butter into a rectangle of about 1 cm thickness, I used a Naturli butter block</li>
+            <li>Rolled out dough into a rectangle twice that size</li>
+            <li>One tour double and one tour simple without rest in between</li>
+            <li>20 minutes rest in the fridge</li>
+            <li>Rolling out dough of rectangle of 20 x 30 cm</li>
+            <li>Cut the dough into 8 croissants of 20 cm length</li>
+            <li>Shaped the croissants, covered with cling film and refrigerate overnight.</li>
+            </ol>
+            
+            <p>Day 3</p>
+            <ol>
+            <li>Prepared the oven as a proofing cabinet by placing a tray with 0.5 L of boiling water at the bottom of the oven and keeping it closed for about 10 minutes for the heat to distribute.</li>
+            <li>Placed the croissants on a baking sheet lined with baking parchment. Placed them in the oven to proof.</li>
+            <li>Proofed for four hours. Took them out, brushed them with barista oat milk.</li>
+            <li>Pre-heated oven to 200 degrees C fan.</li>
+            <li>Put croissants in the oven, turned down heat to 190 degrees.</li>
+            <li>Baked for about 23 minutes until golden brown.</li>
+            </ol>
+            
+            <h2>Observations and results:</h2>
+            <p>The dough was insanely dry. It made it very hard to mix and knead the dough. With hand kneading for 10 minutes I didn't achieve a medium gluten development at all. The dough being this dry also made it quite sturdy, which was good for lamination because it had about the same hardness as the butter at fridge temperature. The layers after touring were very beautiful, continuous and very visible.</p>
+            <p>Proofing went very slow, it should not have lasted four hours. I found out that I made a mistake with the yeast: I didn’t weigh it but just used one package, thinking those were always 7g. But it turns out these were 5g packs, so I didn't use enough yeast. Which explains why the proofing took so long.</p>
+            <p>While proofing there appeared some small rips on the surface of the croissants. This might have been caused by drying out of the dough in the fridge overnight, or most likely because the gluten structure was not developed enough.</p>
+            <p>During the baking, a lot of butter leaked out of the croissants, also a cue that the proofing was not finalized yet. </p>
+            <p>In the oven the croissants grew quite a bit, but they collapsed under their own weight. In the end they didn't have the volume that a good croissant should have, nor the internal structure. The layers were still clearly visible but the dough didn’t have the strength to keep up the high volume.</p>
+            <p>Still lovely to eat though 🙂</p>
+            
+            <h2>What to improve next time:</h2>
+            <ul>
+            <li>Use enough yeast, you dummy</li>
+            <li>Increase the hydration slightly: add 10 grams of milk</li>
+            <li>Knead for longer until the right gluten development is reached</li>
+            </ul>
+            
+            <p>Hopefully that will improve the results.</p>`
+
+        }
+    }
 
 export default blogData;
